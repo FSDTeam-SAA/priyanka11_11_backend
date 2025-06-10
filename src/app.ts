df04881e74eb830479119568c75a83d5312af14e
zihadulislam2 <zihadul708@gmail.com>
2025-06-10 15:15:20 +0600

    feat: implement CORS middleware in app.ts and remove redundant code from server.ts

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,16 @@ import express from 'express'
 import userRoutes from './routes/auth.routes'
 import dataRoutes from './routes/data.routes'
 import errorMiddleware from './middlewares/error.middleware'
-
+import cors from 'cors'
 const app = express()
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+)
+
 
 app.use(express.json())
 
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import app from './app'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db'
-import cors from 'cors'
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
-app.use(
-  cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  })
-)
 
 connectDB().then(() => {
   app.listen(PORT, () => {
