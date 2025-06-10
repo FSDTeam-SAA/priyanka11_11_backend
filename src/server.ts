bbc18cc928e4a68b6780772dae3b6a020e196035
zihadulislam2 <zihadul708@gmail.com>
2025-06-10 15:08:27 +0600

    feat: add CORS support and update dependencies in package.json

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import app from './app'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db'
+import cors from 'cors'
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+)
 
 connectDB().then(() => {
   app.listen(PORT, () => {
